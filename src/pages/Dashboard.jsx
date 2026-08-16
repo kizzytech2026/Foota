b76d0defcc2643b6c8bce6087cf827b7b57c5fa2
gerald Kizito <root@Kizzytech.localdomain>
2026-08-16 19:43:08 +0300

    add shared team context

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,21 @@
+import { useContext } from "react";
+import { TeamContext } from "../context/Teamcontext";
+import Matches from "./Matches";
+
 function Dashboard() {
+    const {team, players, matches} = useContext(TeamContext);
+
     return(
-        <div>
-            <h1>Dashboard</h1>
-            <p>Welcome to Foota</p>
-        </div>
+        <><div>
+            <h1>Welcome to {team.name}</h1>
+            <p>{team.season}</p>
+        </div><div>
+                <h2>{players.length}</h2>
+                <p>Total Players</p>
+            </div><div>
+                <h2>{matches.length}</h2>
+                <p>Total Matches</p>
+            </div></>
     );
 }
 export default Dashboard;
